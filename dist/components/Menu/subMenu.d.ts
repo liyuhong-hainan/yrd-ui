c2725ca32f8dba83244b4eb62fa3c3b81b0fdfda
liyuhong-hainan <820998008@qq.com>
2024-12-24 14:57:46 +0800

    new package

--- dist/components/Menu/subMenu.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export interface SubMenuProps {
-    index?: string;
-    title?: string;
-    className?: string;
-    children?: React.ReactNode;
-}
-export declare const SubMenu: React.FC<SubMenuProps>;
-export default SubMenu;
--- dist/components/Upload/dragger.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from 'react';
-interface DraggerProps {
-    onFile: (files: FileList) => void;
-    children?: ReactNode;
-}
-export declare const Dragger: React.FC<DraggerProps>;
-export default Dragger;
